97e147af04141030c1f568f00cc2419843c1e086
omarr707 <oomarr7@gmail.com>
2022-04-22 14:13:21 +0300

    add new fills

--- jop/admin.py
+++ jop/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Jop
+from .models import Jop ,category
 
 
 admin.site.register(Jop)
+
+admin.site.register(category)
--- jop/models.py
+++ jop/models.py
@@ -17,9 +17,17 @@ class Jop(models.Model):
     vacancv = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
     
     
 
